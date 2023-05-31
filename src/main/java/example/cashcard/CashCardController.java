5308afaeb847c7e6ad3cb7abc4b8748211f66fe7
seshankavisanka <seshankavisanka@gmail.com>
2023-05-31 17:07:45 +0530

    Implementing GET - Create a REST Controller

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class CashCardController {
+    public ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+     }
+}
