da92b25119c2a1b820f76638f1a29c47c2eb9f18
seshankavisanka <seshankavisanka@gmail.com>
2023-05-31 17:08:39 +0530

    Implementing GET - Add the GET endpoint

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/cashcards")
 public class CashCardController {
+
+    @GetMapping("/{requestedId}")
     public ResponseEntity<String> findById() {
-        return ResponseEntity.ok("{}");
-     }
+         return ResponseEntity.ok("{}");
+    }
+    
 }
